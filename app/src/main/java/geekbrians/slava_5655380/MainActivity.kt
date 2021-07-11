dba2f64e245a61a170e565052908a65ce9925c36
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-07-11 15:07:25 +0300

    Создание проекта

---
+++ app/src/main/java/geekbrians/slava_5655380/MainActivity.kt
@@ -0,0 +1,18 @@
+package geekbrians.slava_5655380
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import geekbrians.slava_5655380.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/geekbrians/slava_5655380/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package geekbrians.slava_5655380.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
