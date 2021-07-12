456ca483a5cb167b11b49e147cf412c7a6639f7c
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-07-12 17:24:32 +0300

    Решение заданий 1 и 2 урока 1 (Добавление экрана с фото дня)

---
+++ app/src/main/java/geekbrians/slava_5655380/ui/fragments/pictureoftheday/PictureOfTheDayFragment.kt
@@ -0,0 +1,69 @@
+package geekbrians.slava_5655380.ui.fragments.pictureoftheday
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import coil.api.load
+import geekbrians.slava_5655380.R
+import geekbrians.slava_5655380.databinding.MainFragmentBinding
+import geekbrians.slava_5655380.ui.viewmodels.pictureoftheday.PictureOfTheDayViewModel
+
+class PictureOfTheDayFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+    }
+
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProviders.of(this).get(PictureOfTheDayViewModel::class.java)
+    }
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.getData()
+            .observe(viewLifecycleOwner) { renderData(it) }
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    //Отобразите ошибку
+                    //showError("Сообщение, что ссылка пустая")
+                } else {
+                    //Отобразите фото
+                    //showSuccess()
+                    //Coil в работе: достаточно вызвать у нашего ImageView
+                    //нужную extension-функцию и передать ссылку и заглушки для placeholder
+                    binding.imageView.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                //Отобразите загрузку
+                //showLoading()
+            }
+            is PictureOfTheDayData.Error -> {
+                // pass
+            }
+        }
+    }
+}
--- app/src/main/java/geekbrians/slava_5655380/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package geekbrians.slava_5655380.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
